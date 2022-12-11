805dedae44c948e2979ffbb26e98af54e591b317
Dennis Johns <dennis.johns1011@gmail.com>
2022-12-11 23:01:15 +0530

    updated palindrome.c

--- Cycle-3/palindrome.c
+++ Cycle-3/palindrome.c
@@ -1,59 +1,56 @@
-//Check whether a string is palindrome 
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
-#include <malloc.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
 
-char* stack;
-int top = -1;
+char *stack;
+int tos=-1,tos2,m,n;;
 
-//add element 
-void push(char ele)
-{
-    stack[++top] = ele;
+void push(char item){
+	if(tos == n-1){
+		printf("Stack Overflow");
+		exit(0);	
+	}
+	else{
+	 	stack[++tos]=item;
+	}
 }
 
-//delete element
- char pop()
-{
-    return stack[top--];
+
+char pop(){
+	if(tos < 0){
+		printf("Stack Underflow");
+		exit(0);
+	}
+	else{
+	 	return stack[tos--];
+	}
 }
 
-//Palindrome Function
-int isPalindrome(char str[])
-{
-    int length = strlen(str);
-    //Allocating size for stack
-    stack = (char*)malloc(length * sizeof(char));
-    int i, mid = length / 2;
-    for (i = 0; i < mid; i++) {
-        push(str[i]);
-    }
-    if (length % 2 != 0) {
-        i++;
-    }
-    while (str[i] != '\0') {
-       char ele = pop();
-       if(tolower(ele) != tolower(str[i]))
-       //  if (ele != str[i])
-           return 0;
-       i++;
-    }
-    return 1;
+void checkPalindrome(char str[]){
+	int i=0;
+	while(i < n/2){
+		push(str[i]);
+		i++;
+	}
+	if(n%2 != 0){
+		i++;
+	}
+	while(i< n){
+		if(pop() != str[i]){
+			printf("Not Palindrome");
+			return;
+		}
+		i++;
+	}
+	printf("Palindrome");
 }
 
-//Main Function
-int main()
-{
-    char str[50];
-    printf("Enter a string : ");
-    scanf("%s",str);
-    if (isPalindrome(str)) {
-        printf("Palindrome\n");
-    }
-    else {
-        printf("Not Palindrome\n");
-    }
-     return 0;
+void main(){
+	char str[100];
+	printf("Enter the String: ");
+	gets(str);
+	n = strlen(str);
+	stack = (char*) calloc(n,sizeof(char));
+	checkPalindrome(str); 
 }
