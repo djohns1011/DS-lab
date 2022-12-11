6fd62a2f6a40cf805ef689e45309715fb5afa700
Dennis Johns <dennis.johns1011@gmail.com>
2022-12-11 23:06:59 +0530

    updated stack3.c

--- Cycle-3/stack3.c
+++ Cycle-3/stack3.c
@@ -10,39 +10,27 @@ void Pop();
 void Peek(); 
 void Display(); 
 
-typedef struct{
-    int num;
-}stack;
-
-//Declaring Stack and Auxilary Stack.
-stack s[Max_Size],aux[Max_Size]; 
-int Top=-1;
-int auxTop=-1;
-
-
-//Function to Push
-void push(int element){
-
-    if (Top == Max_Size-1){
-        printf("\nStack is Full\n");
-    }
-    else{
-        s[++Top].num = element;
-        printf("\n%d added to Stack\n",s[Top].num);
-
-        if (auxTop == -1){
-            //if the Auxilary Stack is empty then push the element into it
-            //First element in aux is minimum by default.
-            aux[++auxTop].num=element;
-        }
-        else{
-            //push the element into the Auxilary Stack
-            //if it is less than or equal to the current minimum.
-            if (element<=aux[auxTop].num){
-                aux[++auxTop].num=element;
-            }   
-        }
-    }
+int main() { 
+	int choice; 
+	while(1) { 
+		printf("\nOperations performed by Stack"); 
+		printf("\n1.Push the element\n2.Pop\n3.Peek\n4.Display\n5.Exit\n"); 
+		printf("\n\nEnter the choice:"); 
+		scanf("%d",&choice); 
+		switch(choice) { 
+			case 1: Push(); 
+			break; 
+            		case 2: Pop();
+           		break;
+			case 3: Peek();
+			break; 
+			case 4: Display(); 
+			break; 
+			case 5: exit(0); 
+			break; 
+			default: printf("\nInvalid choice!!"); 
+		} 
+	} 
 }
  
 void Push() { 
@@ -81,55 +69,16 @@ void Pop(){
         printf("Deleted element: %d",element);
     }
 }
-
-//Function to Display Minimum
-void min(){
-    if (auxTop==-1){
-        printf("\nStack is Empty\n");
-    }
-    else{
-        printf("\nThe Minimum Element in the Stack: %d\n",aux[auxTop].num);
-    }   
-}
-
-
-void main(){
-
-    int option;
-    //Continuous For loop
-    while (1){
-        int element;
-        printf("    1 : Push \n");
-        printf("    2 : Pop \n");
-        printf("    3 : Find Minimum Element \n");
-        printf("    4 : Display Stack \n");
-        printf("    5 : Exit \n");
-       
-
-        printf("Enter Your Option: ");
-        scanf("%d",&option);
-
-        switch (option){
-        case 1:
-            printf("\nEnter Element: ");
-            scanf("%d",&element);
-            push(element);
-            break;
-        case 2:
-            pop();
-            break;
-        case 3:
-            min();
-            break;
-        case 4:
-            display();
-            break;
-        case 5:
-            exit(0);
-        
-        default:
-            printf("\n!!Enter a Valid Option!!\n");
-            break;
-        }
-    }
+void Peek() { 
+	printf("Peek (minimum element) : %d",aux[Top2]);
+} 
+void Display() { 
+	if(Top==-1) { 
+		printf("\nUnderflow!!"); 
+	} 
+	else { 
+		printf("\nElements present in the stack: \n"); 
+		for(int i=Top;i>=0;--i) 
+			printf("%d\n",inp_array[i]); 
+	} 
 }
